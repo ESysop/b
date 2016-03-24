cac7cccb13df2dcb0ebfed89e9ed884b957201f0
esysop <leehassel@gmail.com>
2016-03-24 08:55:21 -0500

    starting point, have the 2d list started

---
+++ Problems_b_20160322/Triangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems_b_20160322
+	{
+	public class Trianlges
+		{
+		List<List<string>> XList = new List<List<string>>();
+			List<string> YList = new List<string>();
+			//XList.Add(YList);
+			public int listSize = 5;
+			public void Triangles ()
+			{
+				for (int i = 0; i < listSize; i++)
+					{
+						XList.Add(new List<string>(listSize));
+							for (int i = listSize; i < listSize; i++)
+							{
+
+							}
+
+					}
+			}
+
+			
+		}
+		
+	}
+	
+		
+
+		
+	
